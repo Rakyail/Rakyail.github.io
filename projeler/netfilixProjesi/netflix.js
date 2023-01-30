48c1d7088cf6861c67d97e6bb5260dd5936e8431
engın özdemir <114771064+Rakyail@users.noreply.github.com>
2023-01-30 22:58:40 +0300

    Add files via upload

---
+++ projeler/netfilixProjesi/netflix.js
@@ -0,0 +1,16 @@
+$("#oturumac").click(function (e) { 
+    e.preventDefault();
+    window.location.href="oturum.html"
+    
+});
+$("#button-addon2").click(function (e) { 
+    e.preventDefault();
+    window.location.href="profil.html"
+    
+});
+$("#buton").click(function (e) { 
+    e.preventDefault();
+    window.location.href="profil.html"
+    
+});
+
